b7d015390127dff9bc128bd5a111cc4c23aa6a0a
gaohanqing <gaohanqing@bytedance.com>
2024-12-04 17:33:26 +0800

    feat: test android release

---
+++ src/main/java/engineer/lynx/test/LynxViewShellActivity.java
@@ -0,0 +1,25 @@
+// Copyright 2024 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+package engineer.lynx.test;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class LynxViewShellActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
---
+++ src/main/java/engineer/lynx/test/PlaygroundApplication.java
@@ -0,0 +1,17 @@
+// Copyright 2024 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+package engineer.lynx.test; // Copyright 2024 The Lynx Authors. All rights reserved.
+
+import android.app.Application;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.memory.PoolConfig;
+import com.facebook.imagepipeline.memory.PoolFactory;
+
+public class PlaygroundApplication extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
